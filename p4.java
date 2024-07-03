5f271aa7b7e28e2d908b394b0401ed41652ad663
Bantipatel20 <149317694+Bantipatel20@users.noreply.github.com>
2024-07-03 14:55:46 +0530

    Delete p4.java

--- p4.java
+++
@@ -1,8 +0,0 @@
-public class p4 {
-    public static void main(String[] args) {
-        int[][] arr = {{1},{1,2},{1,2,3}};
-        for(int i=0;i<arr.length;i++){
-                System.out.println("a["+i+"] length "+ arr[i].length); 
-        }  
-    }
-}
